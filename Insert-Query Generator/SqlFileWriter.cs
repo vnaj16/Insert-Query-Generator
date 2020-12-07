c9c8b5aa9d8bd40905aed1fca7cfaa04155618e3
Arthur <arthur1610@live.com>
2020-12-06 19:34:03 -0500

    File begin

---
+++ Insert-Query Generator/SqlFileWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insert_Query_Generator
+{
+    public class SqlFileWriter//Aca se le pasara la ruta y el contenido a escribir en un archivo .sql
+
+        //GUIARSE DEL LIBRO C# / FILE HANDLING / Reading and Writing File
+    {
+    }
+}
